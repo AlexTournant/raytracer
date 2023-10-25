a1140161fc35620488bf073b2871ed5e20ee13e2
Corentin Fournier <corentin_fournier2@ens.univ-artois.fr>
2023-10-25 11:24:02 +0200

    ✨Création de getMyImage

--- src/main/java/Model/RayThrower.java
+++ src/main/java/Model/RayThrower.java
@@ -8,34 +8,50 @@ import java.awt.image.BufferedImage;
 import static java.lang.Math.abs;
 
 public class RayThrower {
-    Camera cam;
-    Scene scene;
-    int imgwidth, imgheight;
-    public RayThrower(Camera cam, Scene scene, int x, int y){
-            this.cam = cam;
+    private Camera cam;
+    private Scene scene;
+    private int imgwidth, imgheight;
+    public RayThrower(Scene scene, int x, int y){
             this.scene = scene;
+            this.cam = scene.getCamera();
             this.imgwidth = x;
             this.imgheight = y;
     }
 
-    public Vector orthonormalW(Camera cam){
-        Triplet temp = cam.getLookFrom();
+    public int getImgwidth() {
+        return imgwidth;
+    }
 
-        Triplet temp2 = cam.getLookAt();
+    public int getImgheight() {
+        return imgheight;
+    }
 
-        Vector w1 = new Vector(cam.getLookFrom().subtract(cam.getLookAt()));
+    public Camera getCam() {
+        return cam;
+    }
+
+    public Scene getScene() {
+        return scene;
+    }
+
+    public Vector orthonormalW(){
+        Triplet temp = getCam().getLookFrom();
+
+        Triplet temp2 = getCam().getLookAt();
+
+        Vector w1 = new Vector(getCam().getLookFrom().subtract(getCam().getLookAt()));
         Vector w2 = new Vector(temp.subtract(temp2));
         Vector w = new Vector((temp.divide(temp2)).normalize());
         return w;
     }
 
-    public Vector orthonormalU(Camera cam){
-        Triplet tmp = orthonormalW(cam).getTriplet();
-        Triplet u1 = cam.getUp().multiply(tmp);
+    public Vector orthonormalU(){
+        Triplet tmp = orthonormalW().getTriplet();
+        Triplet u1 = getCam().getUp().multiply(tmp);
 
-        Triplet upTemp = cam.getUp();
+        Triplet upTemp = getCam().getUp();
 
-        Triplet TempW = orthonormalW(cam).getTriplet();
+        Triplet TempW = orthonormalW().getTriplet();
 
 
         Triplet u2 = upTemp.multiply(TempW).normalize();
@@ -44,11 +60,21 @@ public class RayThrower {
         return y;
     }
 
-    public Vector orthonormalV(Camera cam){
-        Vector v1 = orthonormalW(cam).multiply(orthonormalU(cam));
-        Vector v2 = orthonormalW(cam).multiply(orthonormalU(cam)).normalize();
+    public Vector orthonormalV(){
+        Vector v1 = orthonormalW().multiply(orthonormalU());
+        Vector v2 = orthonormalW().multiply(orthonormalU()).normalize();
         Vector v = new Vector(v1.getTriplet().divide(v2.getTriplet()));
         return v;
+    }
+
+    public BufferedImage getMyImage(){
+        BufferedImage image = new BufferedImage(this.getImgwidth(), this.getImgheight(), BufferedImage.TYPE_INT_ARGB);
+        for (int x=0; x<getImgwidth(); x++){
+            for (int y=0; y<getImgheight(); y++){
+                image.setRGB(x, y, 0);
+            }
+        }
+        return image;
     }
 
     public void SaveImage() throws Exception{
