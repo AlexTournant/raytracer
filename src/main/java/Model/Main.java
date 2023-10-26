8b967700cb5a9e1c89eacc47c1d1a47c73cd74f8
Alex Tournant <alex.tournant.1@gmail.com>
2023-10-26 09:46:57 +0200

    🚑️ Possibiliter de lancer plusieur fichier a la suite dans le main

--- src/main/java/Model/Main.java
+++ src/main/java/Model/Main.java
@@ -7,19 +7,25 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         //String fileName = args[0];
-        Parser p=new Parser();
+        ArrayList<String> files=new ArrayList<>();
+        files.add("mickey.txt");
+        files.add("2SphereDiff.txt");
+        files.add("2Sphere.txt");
         //p.parse(fileName);
-        p.parse("mickey.txt");
-        p.addCamera();
-        p.addImage();
-        ArrayList<IObjetScene> listObjetScene = new ArrayList<>();
-        listObjetScene.addAll(p.getSpheres());
-        listObjetScene.addAll(p.getTriangles());
-        listObjetScene.addAll(p.getPlans());
-        Scene.SceneBuilder sb=new Scene.SceneBuilder().withCamera(p.getCameras().get(0)).withColors(p.getColors()).withObjets(listObjetScene).withImage(p.getImage().get(0));
-        Scene scene= sb.build();
-        RayThrower rt=new RayThrower(scene);
-        rt.rayTracing();
+        for (String file:files){
+            Parser p=new Parser();
+            p.parse(file);
+            p.addCamera();
+            p.addImage();
+            ArrayList<IObjetScene> listObjetScene = new ArrayList<>();
+            listObjetScene.addAll(p.getSpheres());
+            listObjetScene.addAll(p.getTriangles());
+            listObjetScene.addAll(p.getPlans());
+            Scene.SceneBuilder sb = new Scene.SceneBuilder().withCamera(p.getCameras().get(0)).withColors(p.getColors()).withObjets(listObjetScene).withImage(p.getImage().get(0));
+            Scene scene = sb.build();
+            RayThrower rt = new RayThrower(scene);
+            rt.rayTracing();
+        }
     }
 
 }
