3a03df24ca1c72b0e4ab99d7398cdb8e0ccc3c42
DEMORY Mael <mael_demory@ens.univ-artois.fr>
2023-10-27 14:30:54 +0200

    🐛 Correction de l'implémentation du patron de conception builder pour que SceneBuilder soit dans un autre fichier

--- src/main/java/Model/Main.java
+++ src/main/java/Model/Main.java
@@ -6,11 +6,9 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        //String fileName = args[0];
         ArrayList<String> files=new ArrayList<>();
         files.add("mickey.txt");
         files.add("2Sphere.txt");
-        //p.parse(fileName);
         for (String file:files){
             Parser p=new Parser();
             p.parse(file);
@@ -20,7 +18,7 @@ public class Main {
             listObjetScene.addAll(p.getSpheres());
             listObjetScene.addAll(p.getTriangles());
             listObjetScene.addAll(p.getPlans());
-            Scene.SceneBuilder sb = new Scene.SceneBuilder().withCamera(p.getCameras().get(0)).withColors(p.getColors()).withObjets(listObjetScene).withImage(p.getImage().get(0));
+            SceneBuilder sb = new SceneBuilder().withCamera(p.getCameras().get(0)).withColors(p.getColors()).withObjets(listObjetScene).withImage(p.getImage().get(0));
             Scene scene = sb.build();
             RayThrower rt = new RayThrower(scene);
             rt.rayTracing();
