043ad06d46fda5d6ab0d4c444442fb98db5df923
Alex Tournant <alex.tournant.1@gmail.com>
2023-10-28 11:23:58 +0200

    :sparkles: implementation de l'interface Builder pour respecter le patron de conception Builder

--- src/main/java/Model/Scene.java
+++ src/main/java/Model/Scene.java
@@ -5,62 +5,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class Scene {
-    private Camera camera;
-    private Image image;
-    private Map<String,Color> colors;
-    private ArrayList<IObjetScene> objets ;
-    private ArrayList<ILight> lights;
-    private ArrayList<DirectionalLight>Dlights;
-    public static class SceneBuilder {
-        private Camera camera=new Camera(null,null,null,0);
-        private Image image=new Image(600,600,"image.png");
-        private Map<String,Color> colors=new HashMap<>();
-        private ArrayList<DirectionalLight>Dlights=new ArrayList<>();
+    private final Camera camera;
+    private final Image image;
+    private final Map<String,Color> colors;
+    private final Map<IObjetScene, Color> objets;
+    private final ArrayList<ILight> lights;
 
-        private ArrayList<IObjetScene> objets = new ArrayList<>();
-
-        private ArrayList<ILight> lights=new ArrayList<>();
-
-        public SceneBuilder withCamera(Camera cam) {
-            this.camera = cam;
-            return this;
-        }
-
-        public SceneBuilder withImage(Image img) {
-            this.image = img;
-            return this;
-        }
-
-        public SceneBuilder withColors(Map<String,Color> colors) {
-            this.colors = colors;
-            return this;
-        }
-
-        public SceneBuilder withObjets(ArrayList<IObjetScene> objets) {
-            this.objets = objets;
-            return this;
-        }
-
-        public SceneBuilder withLights(ArrayList<ILight> lights){
-            this.lights=lights;
-            return this;
-        }
-
-        public SceneBuilder withDLights(ArrayList<DirectionalLight> Dlights){
-            this.Dlights=Dlights;
-            return this;
-        }
-
-        public Scene build() {
-            Scene sc=new Scene();
-            sc.camera=this.camera;
-            sc.image=this.image;
-            sc.colors=this.colors;
-            sc.objets=this.objets;
-            sc.lights=this.lights;
-            sc.Dlights=this.Dlights;
-            return sc;
-        }
+    public Scene(Camera camera, Image image, Map<String, Color> colors,Map<IObjetScene, Color> objets, ArrayList<ILight> lights) {
+        this.camera = camera;
+        this.image = image;
+        this.colors = colors;
+        this.objets = objets;
+        this.lights = lights;
     }
 
     public Camera getCamera() {
@@ -75,15 +31,11 @@ public class Scene {
         return colors;
     }
 
-    public ArrayList<IObjetScene> getObjets() {
+    public Map<IObjetScene, Color> getObjets() {
         return objets;
     }
 
     public ArrayList<ILight> getLights() {
         return lights;
-    }
-
-    public ArrayList<DirectionalLight> getDlights() {
-        return Dlights;
     }
 }
---
+++ src/main/java/Model/SceneBuilder.java
@@ -0,0 +1,41 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class SceneBuilder implements Builder{
+    private Camera camera;
+    private Image image;
+    private Map<String, Color> colors;
+
+    private Map<IObjetScene,Color> objets=new LinkedHashMap<>();
+
+    private ArrayList<ILight> lights;
+
+    @Override
+    public void withCamera(Camera cam) {
+        this.camera = cam;
+    }
+    @Override
+    public void withImage(Image img) {
+        this.image = img;
+    }
+    @Override
+    public void withColors(Map<String,Color> colors) {
+        this.colors = colors;
+    }
+    @Override
+    public void withObjets(Map<IObjetScene, Color> objets) {
+        this.objets = objets;
+    }
+
+    @Override
+    public void withLights(ArrayList<ILight> lights){
+        this.lights=lights;
+    }
+    @Override
+    public Scene build() {
+        return new Scene(this.camera,this.image,this.colors,this.objets,this.lights);
+    }
+}
