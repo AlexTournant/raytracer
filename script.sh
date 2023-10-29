727871c2023c7dbc9d41b515e79d4479e28660e8
Alex Tournant <alex.tournant.1@gmail.com>
2023-10-30 00:07:01 +0100

    update

--- script.sh
+++ script.sh
@@ -1,3 +1,7 @@
-gradle run --args=$@
-#./script.sh 'mystere1.txt' 'mystere2.txt' 'mystere3.txt'
-#$@= 'mystere1.txt' 'mystere2.txt' 'mystere3.txt'
+tab=()
+occ=0
+for i in $@; do
+  gradle run --args="'"$i"'"
+  done
+#./script.sh ['mystere1.txt','mystere2.txt','mystere3.txt']
+#$@= ['mystere1.txt','mystere2.txt','mystere3.txt']
